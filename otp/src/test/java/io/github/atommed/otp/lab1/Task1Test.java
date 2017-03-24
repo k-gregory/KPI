359ba980d0ed560beefc75bee67163fb6e61dd59
Gregory K <xalvevs@gmail.com>
2017-03-25 00:44:49 +0200

    OTP Lab 1: Add file content test

--- otp/src/test/java/io/github/atommed/otp/lab1/Task1Test.java
+++ otp/src/test/java/io/github/atommed/otp/lab1/Task1Test.java
@@ -1,9 +1,13 @@
 package io.github.atommed.otp.lab1;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class Task1Test{
@@ -13,8 +17,10 @@ public class Task1Test{
     }
 
     @Test
-    public void task1() {
+    public void task1() throws IOException {
         InputStream input = getClass().getResourceAsStream("/lab1/task1.txt");
+        String str = IOUtils.toString(input, StandardCharsets.UTF_8);
+        assertEquals(str.split("\n")[0],"this");
     }
 }
 
