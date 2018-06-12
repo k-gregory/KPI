c1a3ed89393baaedece37b9e75daced0c279f03d
Gregory Komar <gregory_k@hotmail.com>
2018-06-12 11:02:54 +0300

    First Newton

--- numerical-methods/src/app/newton-first/newton-first.component.ts
+++ numerical-methods/src/app/newton-first/newton-first.component.ts
@@ -1,15 +1,115 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {decodeCsv, getControls} from '../csv'
+import {newton_first} from "../newton";
+import {ChartDataSets, ChartOptions} from "chart.js";
+
 
 @Component({
   selector: 'app-newton-first',
   templateUrl: './newton-first.component.html',
   styleUrls: ['./newton-first.component.css']
 })
-export class NewtonFirstComponent implements OnInit {
+export class NewtonFirstComponent {
+  error?: string;
+
+  inputFunction?: {x: number, y: number}[];
+  controlFunction: {x: number, y: number}[];
+
+  selectedPoint: {x: number, y: number};
+
+  point?: string;
+  result?: number;
+
+  colors: any;
+  chartDss? : ChartDataSets[] = null;
+
 
   constructor() { }
 
-  ngOnInit() {
+  options : ChartOptions = {
+    tooltips: {
+      enabled: true
+    }
+  };
+
+  updateDss(){
+    const base : ChartDataSets = {
+      label: 'Вхідна функція',
+      data: this.inputFunction,
+      showLine: true
+    };
+
+    const cls = [{
+      pointBackgroundColor: 'blue'
+    }];
+    const res: ChartDataSets[] = [base];
+
+    if(this.controlFunction){
+      res.push({
+        label: 'Контрольні точки',
+        data: this.controlFunction,
+      });
+      cls.push({
+        pointBackgroundColor: 'rgba(255, 0, 0)'
+      });
+    }
+
+    if(this.selectedPoint){
+      console.log("x");
+      res.push({
+        label: 'Обрана точка',
+        data: [this.selectedPoint]
+      });
+      cls.push({
+        pointBackgroundColor: 'rgba(0, 255, 0)'
+      });
+    }
+
+
+    this.chartDss = null;
+    this.colors = null;
+    setTimeout(()=>{
+      this.chartDss = res;
+      this.colors = cls;
+    }, 1)
+  }
+
+
+  inputSelected(csv: string){
+    this.result = null;
+    this.error = null;
+    this.inputFunction = null;
+
+    try{
+      this.inputFunction = decodeCsv(csv);
+      this.updateDss();
+    } catch (e) {
+      this.error = e.toString();
+    }
+  }
+
+  controlSelected(csv: string){
+    this.controlFunction = getControls(csv).map(x=>({
+      x, y: newton_first(this.inputFunction, x)
+    }));
+    console.log(this.controlFunction);
+    this.updateDss();
+  }
+
+  calc(){
+    const x = parseFloat(this.point);
+    if(isNaN(x)){
+      this.error="Can't parse input point";
+      this.selectedPoint = null;
+    } else {
+      this.error = null;
+      this.selectedPoint = {
+        x,
+        y:newton_first(this.inputFunction, x),
+      };
+      this.result = this.selectedPoint.y;
+      this.updateDss();
+    }
   }
 
 }
--- numerical-methods/src/app/newton.ts
+++ numerical-methods/src/app/newton.ts
@@ -1,3 +1,90 @@
+export class FirstNewton {
+  _values: { x: number, y: number }[];
+
+  constructor(_values: { x: number, y: number }[]) {
+    this._values = [..._values];
+    /*this._values.sort((a,b)=>(b.x - a.x))*/
+    this._values.sort((a,b)=>(a.x - b.x))
+  }
+
+  _cache: { [key: string]: number } = {};
+
+  private CalculateBase(point: number): number {
+    let baseValue = Number.MAX_VALUE;
+    let baseIndex = -1;
+    for (let i = 0; i < this._values.length; i++) {
+      const value = this._values[i].x;
+      if (Math.abs(point - value) < Math.abs(point - baseValue)) {
+        baseValue = value;
+        baseIndex = i;
+      }
+    }
+
+    return baseIndex;
+  }
+
+
+  private PopulateCache(baseIndex: number): number {
+    const d = 100;
+    const epsilon = 5 * Math.pow(10, -d);
+
+    for (let length = 1; length + baseIndex < this._values.length; length++) {
+      let isLargerThanEpsilon = false;
+
+      for (let from = baseIndex; from + length < this._values.length; from++) {
+        let delta = this.CalculateDelta(from, length);
+        this._cache[FirstNewton.GetCacheKey(from, length)] = delta;
+        if (Math.abs(delta) > epsilon)
+          isLargerThanEpsilon = true;
+      }
+
+      if (!isLargerThanEpsilon)
+        return length;
+    }
+
+    return this._values.length - baseIndex;
+  }
+
+  private CalculateDelta(from: number, length: number): number {
+    if (length == 1)
+      return this._values[from].y;
+
+    return (this.CachedDelta(from + 1, length - 1) - this.CachedDelta(from, length - 1))
+      / (this._values[from + length - 1].x - this._values[from].x);
+  }
+
+  private static GetCacheKey(from: number, length: number): string {
+    return `${from}: ${length}`;
+  }
+
+  private CachedDelta(from: number, length: number): number {
+    return this._cache[FirstNewton.GetCacheKey(from, length)];
+  }
+
+  private static Factorial(i: number): number {
+    let result = 1;
+    for (; i != 1; i--)
+      result = result * i;
+    return result;
+  }
+
+  public Interpolate(point: number) : number {
+    const baseIndex = this.CalculateBase(point);
+    const stoppedAt = this.PopulateCache(baseIndex);
+
+    let acc = this._values[baseIndex].y;
+    for(let length = 2; length < stoppedAt; length++){
+      let xAcc = 1;
+      for(let cur = 0; cur < length; cur++){
+        xAcc *= (point - this._values[cur + baseIndex].x)
+      }
+      acc += this.CachedDelta(baseIndex, length) * xAcc / FirstNewton.Factorial(length);
+    }
+
+    return acc;
+  }
+}
+
 export class SecondNewtonEvenly {
   _values: { x: number, y: number }[];
 
@@ -70,31 +157,25 @@ export class SecondNewtonEvenly {
   public Interpolate(point: number) : number {
     const baseIndex = this.CalculateBase(point);
     const stoppedAt = this.PopulateCache(baseIndex);
-    console.log(this._cache);
 
-    const range = (start, count) => [...Array(count).map(k=>k+start)];
+    let acc = this._values[baseIndex].y;
+    for(let length = 2; length < stoppedAt; length++){
+      let xAcc = 1;
+      for(let cur = 0; cur < length; cur++){
+        xAcc *= (point - this._values[cur + baseIndex].x)
+      }
+      acc += this.CachedDelta(baseIndex, length) * xAcc / SecondNewtonEvenly.Factorial(length);
+    }
 
-    const lengths = stoppedAt < 2 ? [] : range(2, stoppedAt - 2);
-
-    const reduce = lengths.reduce((acc, length)=>
-      acc + this.CachedDelta(baseIndex, length) * range(0, length - 1)
-        .reduce((xAcc, cur)=> xAcc * (point - this._values[cur + baseIndex].x), 1) / SecondNewtonEvenly.Factorial(length), this._values[baseIndex].y);
-    console.log(reduce);
-    return reduce;
-
-
-    /*
-      var lengths = stoppedAt < 2 ? Enumerable.Empty<int>() : Enumerable.Range(2, stoppedAt - 2);
-
-      Result = lengths
-        .Aggregate<int, double>(_values[baseIndex].Y, (acc, length) => acc
-          + CachedDelta(baseIndex, length)
-          * Enumerable.Range(0, length - 1).Aggregate<int, double>(1, (xAcc, cur) => xAcc * (point - _values[cur + baseIndex].X)) / Factorial(length));
-          * */
+    return acc;
   }
 }
 
 
 export function newton_second_evenly(f: { x: number, y: number }[], x: number) {
   return new SecondNewtonEvenly(f).Interpolate(x);
+}
+
+export function newton_first(f: {x: number, y: number}[], x: number){
+  return new FirstNewton(f).Interpolate(x);
 }
